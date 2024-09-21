c20844e6fc5f489b07d13cf4556a55c2c4ef507c
ryanhuge-ryjhen <nayreuqalam@gmail.com>
2024-09-21 21:38:17 +0800

    added header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./*html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        primary: "#FD3D57",
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+        roboto: ["Roboto"],
+      },
+    },
   },
   plugins: [],
 };
